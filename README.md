c9a21fd65cd31740506880061143332b040a895a
Brian Sorahan <bsorahan@leaf.ag>
2016-10-29 08:42:02 -0500

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # scfree
+
 Free all synths in a SuperCollider server
+
+## Install
+
+`go get github.com/scgolang/scfree`
+
+## Usage
+
+`scfree`
+
+To see a help message with command line flags,
+
+`scfree -h`
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/scgolang/sc"
+)
+
+func main() {
+	var (
+		port int
+		host string
+	)
+
+	flag.IntVar(&port, "p", 57120, "scsynth port")
+	flag.StringVar(&host, "h", "127.0.0.1", "scsynth host")
+
+	flag.Parse()
+
+	serverAddr := fmt.Sprintf("%s:%d", host, port)
+
+	client, err := sc.NewClient("udp", "127.0.0.1:57110", serverAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// TODO: free other groups as well?
+	if err := client.FreeAll(sc.DefaultGroupID); err != nil {
+		log.Fatal(err)
+	}
+}
